39a1176b5cd1ef615da657fcd153d65517822907
s7xs94pnd <dastanovdanil0207@gmail.com>
2024-11-22 08:57:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/inote/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inote.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.inote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
